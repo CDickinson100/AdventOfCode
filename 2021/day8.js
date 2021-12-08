74b9f940f23f7f361a6064b191b9a73e38cfcd90
Chris Dickinson <chris.dickinson@iress.com>
2021-12-08 21:44:18 +0000

    Refactored Day 8

--- 2021/day8.js
+++ 2021/day8.js
@@ -20,100 +20,47 @@ const display = {
 }
 
 function part1() {
-    return input.map(value => value[1])
-        .reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
-        .filter(value => value.length === display["1"].length || value.length === display["4"].length || value.length === display["7"].length || value.length === display["8"].length)
-        .length;
+    return getSegments()
+        .reduce((previous, current) => previous.concat(current), [])
+        .filter(value => value === 1 || value === 4 || value === 7 || value === 8).length;
 }
 
 function part2() {
-    let count = 0;
-    for (let i = 0; i < input.length; i++) {
-        console.log(i + "/" + input.length);
-        const decodeKey = decode(input[i][0]);
-        const numbers = input[i][1]
-            .map(value => value.split("").map(character => decodeKey[character]))
-            .map(value => getSignal(value))
-            .join("");
-        count += parseInt(numbers);
-    }
-    return count;
+    return getSegments()
+        .map(value => parseInt(value.join("")))
+        .reduce((previous, current) => previous + current, 0);
+}
+
+function getSegments() {
+    return input.map(signal => signal[1]
+        .map(value => value.split("").sort())
+        .map(value => [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(number => decode(signal[0])[number].join(",") === value.join(",")))
+        .map(value => value.length === 1 ? value[0] : 0));
 }
 
 function decode(input) {
-    const decoded = getPotentialCharacters(input);
+    input = input.map(value => value.split(""));
+    const one = input.filter(value => value.length === 2)[0];
+    const seven = input.filter(value => value.length === 3)[0];
+    const four = input.filter(value => value.length === 4)[0];
+    const eight = input.filter(value => value.length === 7)[0];
+    const six = input.filter(value => value.length === 6).filter(value => value.filter(x => one.includes(x)).length === 1)[0];
+    const five = input.filter(value => value.length === 5).filter(value => value.filter(x => !six.includes(x)).length === 0)[0];
+    const nine = input.filter(value => value.length === 6).filter(value => four.filter(x => value.includes(x)).length === 4)[0];
+    const two = input.filter(value => value.length === 5).filter(value => value !== nine).filter(value => value.filter(x => !nine.includes(x)).length === 1)[0];
+    const three = input.filter(value => value.length === 5).filter(value => value !== five).filter(value => value !== two)[0];
 
-    const decodeIndex = new Array(7).fill(0);
-    while (true) {
-        const currentDecoding = {
-            a: decoded.a[decodeIndex[0]],
-            b: decoded.b[decodeIndex[1]],
-            c: decoded.c[decodeIndex[2]],
-            d: decoded.d[decodeIndex[3]],
-            e: decoded.e[decodeIndex[4]],
-            f: decoded.f[decodeIndex[5]],
-            g: decoded.g[decodeIndex[6]]
-        }
-        if (input.map(value => value.split(""))
-            .map(value => value.map(character => currentDecoding[character]))
-            .map(value => getSignal(value).length === 1)
-            .filter(value => !value)
-            .length === 0) {
-            return currentDecoding;
-        }
-        decodeIndex[0]++;
-        let currentIndex = 0;
-        let currentCharacter = "a";
-        while (decodeIndex[currentIndex] === decoded[currentCharacter].length) {
-            decodeIndex[currentIndex] = 0;
-            decodeIndex[currentIndex + 1]++;
-            currentIndex++;
-            currentCharacter = next(currentCharacter);
-        }
+    return {
+        1: one.sort(),
+        2: two.sort(),
+        3: three.sort(),
+        4: four.sort(),
+        5: five.sort(),
+        6: six.sort(),
+        7: seven.sort(),
+        8: eight.sort(),
+        9: nine.sort(),
     }
-}
-
-function next(character) {
-    if (character === "a") return "b";
-    if (character === "b") return "c";
-    if (character === "c") return "d";
-    if (character === "d") return "e";
-    if (character === "e") return "f";
-    if (character === "f") return "g";
-    if (character === "g") return "h";
-}
-
-function getSignal(signal) {
-    return Object.keys(display)
-        .filter(value => display[value].sort().join(",") === signal.sort().join(","));
-}
-
-function getPotentialCharacters(input) {
-    const decoded = {
-        a: ["a", "b", "c", "d", "e", "f", "g"],
-        b: ["a", "b", "c", "d", "e", "f", "g"],
-        c: ["a", "b", "c", "d", "e", "f", "g"],
-        d: ["a", "b", "c", "d", "e", "f", "g"],
-        e: ["a", "b", "c", "d", "e", "f", "g"],
-        f: ["a", "b", "c", "d", "e", "f", "g"],
-        g: ["a", "b", "c", "d", "e", "f", "g"]
-    }
-
-    input.forEach(signal => {
-        const signals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-            .map(value => display[value])
-            .filter(value => value.length === signal.length)
-            .reduce((previousValue, currentValue) => previousValue.concat(currentValue), [])
-            .filter((value, index, array) => array.indexOf(value) === index)
-
-        signal.split("").forEach(character => {
-            if (decoded[character].length > signals.length) {
-                decoded[character] = signals;
-            }
-        });
-    });
-
-    return decoded;
 }
 
 console.log(part1());
