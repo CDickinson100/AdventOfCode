a23d019315e34d98862cef80712dbbcc3e8670b5
Chris Dickinson <chris.dickinson@iress.com>
2021-12-08 21:44:54 +0000

    Refactored Day 8

--- 2021/day8.js
+++ 2021/day8.js
@@ -6,19 +6,6 @@ const [...input] = fs
     .filter(value => value)
     .map(value => value.split(" | ").map(value1 => value1.split(" ")))
 
-const display = {
-    0: ["a", "b", "c", "e", "f", "g"],
-    1: ["c", "f"],
-    2: ["a", "c", "d", "e", "g"],
-    3: ["a", "c", "d", "f", "g"],
-    4: ["b", "c", "d", "f"],
-    5: ["a", "b", "d", "f", "g"],
-    6: ["a", "b", "d", "e", "f", "g"],
-    7: ["a", "c", "f"],
-    8: ["a", "b", "c", "d", "e", "f", "g"],
-    9: ["a", "b", "c", "d", "f", "g"]
-}
-
 function part1() {
     return getSegments()
         .reduce((previous, current) => previous.concat(current), [])
